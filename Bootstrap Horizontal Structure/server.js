0352ec999357c46bb386fb56f3d336d7f5ed639b
Anar Enhsaihan <anar.enhsaihan@cittacorp.com>
2015-04-06 23:18:16 -0400

    Experiment with process.env.NODE_ENV environment variable

--- Bootstrap Horizontal Structure/server.js
+++ Bootstrap Horizontal Structure/server.js
@@ -1,3 +1,7 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+console.log('Environment is running in ' + process.env.NODE_ENV);
+
 var express = require('./config/express');
 
 var app = express();
