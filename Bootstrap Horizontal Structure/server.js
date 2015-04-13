b244275d28d50c42adec08a2a220258246664809
Anar Enhsaihan <anar.enhsaihan@cittacorp.com>
2015-04-13 16:05:40 -0400

    Configure passport

--- Bootstrap Horizontal Structure/server.js
+++ Bootstrap Horizontal Structure/server.js
@@ -3,10 +3,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 console.log('Environment is running in ' + process.env.NODE_ENV);
 
 var mongoose = require('./config/mongoose'),
-    express = require('./config/express');
+    express = require('./config/express'),
+    passport = require('./config/passport');
 
 var db = mongoose();
 var app = express();
+var passport = passport();
+
 app.listen(3000);
 module.exports = app;
 
